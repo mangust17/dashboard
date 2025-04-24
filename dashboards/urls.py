07ff62c51e5de1d313b0591cdb219fffdcecc01d
mangust <matvejlejko5@gmail.com>
2025-04-24 15:04:32 +0300

    Добавил логин и рассказал про графики

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('y2y', dash_graph, name='y2y'),
-    path('', plotly_dash, name='home'),
+    path('', login_view, name='login'),
+    path('info', info, name='info'),
+    path('dash', plotly_dash, name='home'),
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def dash_graph(request):
     return render(request, "dashboards/graph.html")
@@ -7,3 +9,22 @@ def dash_graph(request):
 def plotly_dash(request):
     context = {}
     return render(request, "dashboards/plotly_dash.html", context)
+
+def info(request):
+    return render(request, "info.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Неверный логин или пароль')
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
