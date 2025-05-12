621604ec6a8e39432c0b770f8be9747d2f56fcaa
AlexTr <vaskaop@mail.ru>
2025-05-12 12:37:37 +0300

    Поднял на уровень выше

--- dashsite/settings.py
+++ dashsite/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 print('базовая')
 print(BASE_DIR)
 
